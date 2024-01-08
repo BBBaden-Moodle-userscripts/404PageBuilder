85e1c3cd7b3da81ac331c0d96260c8b1ce8a4513
PianoNic <79938743+Pianonic@users.noreply.github.com>
2024-01-08 10:21:10 +0100

    Update 404PageBuilder.lib.user.js

--- 404PageBuilder.lib.user.js
+++ 404PageBuilder.lib.user.js
@@ -2,7 +2,7 @@ var PageBuilder = (function () {
     
     function info (){
         const name = "404PageBuilder.lib.user.js";
-        const version = "0.2";
+        const version = "0.3";
         const description = "edits 404 pages from moodle.bbbaden.ch";
         const author = "PianoNic";
         const homepageURL = "";
