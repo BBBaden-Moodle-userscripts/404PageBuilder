0b9f3943ad900706f82196d6b4a2040b5d5366a0
PianoNic <79938743+Pianonic@users.noreply.github.com>
2024-01-08 09:09:10 +0100

    Create 404PageBuilder.lib.user.js

---
+++ 404PageBuilder.lib.user.js
@@ -0,0 +1,22 @@
+var MyTableUtils = (function () {
+    function info (){
+        const name = "extract.lib.user.js";
+        const version = "1.0.0";
+        const description = "processes tables from moodle.bbbaden.ch";
+        const author = "black-backdoor";
+        
+        const homepageURL = ""
+
+        return {
+            name: name,
+            version: version,
+            description: description,
+            author: author,
+            homepageURL: homepageURL   
+        };
+    }
+
+    return {
+      info: info
+    };
+})();
