8469fa6912d5305da343b9c84b3d9607802875a9
PianoNic <79938743+Pianonic@users.noreply.github.com>
2024-01-08 14:44:43 +0100

    Update 404PageBuilder.lib.user.js

--- 404PageBuilder.lib.user.js
+++ 404PageBuilder.lib.user.js
@@ -57,19 +57,25 @@ var PageBuilder = (function () {
         `
     }
         
-    function addButton(text, jsCode) {
-        var pageContent = document.getElementsByClassName('custom-content')[0];
-    
-        var button = document.createElement("button");
-        button.innerHTML = text;
-    
-        button.addEventListener('click', function() {
-            eval(jsCode);
-        });
-    
-        pageContent.appendChild(button);
-        return button;
-    } 
+    function addButton(buttonText, jsCode) {
+        // Check if the div with class 'custom-content' exists
+        const customContentDiv = document.querySelector('.custom-content');
+
+        if (customContentDiv) {
+            // Create button element
+            const button = document.createElement('button');
+            button.innerText = buttonText || 'Click me!';
+
+            // Add click event listener to the button
+            button.addEventListener('click', function() {
+                // Execute custom JavaScript code passed as a string
+                eval(jsCode); // Be cautious with eval, it can execute arbitrary code
+            });
+
+            // Append button to custom-content div
+            customContentDiv.appendChild(button);
+        }
+    }
 
     function addTextField(id){
         var pageContent = document.getElementsByClassName('custom-content')[0];
